0e9b1766b73a00333d49f87e65a14271280c6bd0
kacperoleszkiewicz <65919179+kacperoleszkiewicz@users.noreply.github.com>
2022-09-27 07:33:48 +0200

    Add files via upload

---
+++ read_pdf.py
@@ -0,0 +1,29 @@
+# I tried PyPDF2, pdfminer, pymupdf , tika , PDFium. 
+
+
+"""
+to make below work, I installed Choco, and followed the official documnetation at https://ocrmypdf.readthedocs.io/en/latest/
+I have installed ocrmypdf with "pip install ocrmypdf --user"
+
+"""
+import ocrmypdf
+
+def ocr(file_path, save_path):
+    ocrmypdf.ocr(file_path, save_path, force_ocr=True, l="pol")
+
+print('We are here')
+ocr(r"C:\Users\ignacy elka\OneDrive\Pulpit\Kacper\Web Fundacja Scrapping\asd.pdf", r"C:\Users\ignacy elka\OneDrive\Pulpit\Kacper\Web Fundacja Scrapping\normal2.pdf")
+
+
+"""
+1.point is to do that for next 50 PDFs
+    once PDF is readable, either check if ocr my PDF is storing the text in a string that can be called
+    if not, open and read the whole document with file reader
+    (if that does not work store output file in .txt format)
+
+    Get the data that interest you (name, year, amount of donations received, amount of donations spent on statutory cause)
+
+    Save the file in organisation name - year convention   
+
+
+"""
